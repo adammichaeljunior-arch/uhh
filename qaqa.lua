9597adbfa409e200fcd698622543fe53309fd9a2
adammichaeljunior-arch <adammichaeljunior@gmail.com>
2025-10-08 15:47:45 -0400

    Update qaqa.lua

--- qaqa.lua
+++ qaqa.lua
@@ -1,6 +1,15 @@
 -- === SETTINGS ===
 local WEBHOOK_URL = "https://discord.com/api/webhooks/1423446494152884295/rip25iG9fUAoY63CE5uYRqpKNeNz5HJoS0jTH0X4CRpXkS2hJqBk6xn8KLq1yNu_BHxI"
 
+if setfpscap then
+    setfpscap(20) -- Change this value to your desired FPS
+else
+    warn("Executor does not support setfpscap!")
+end
+--by chat gpt ;)
+
+
+
 local messages = {
     "hop in /|oIz for ekittens",
     "bored?? /|oIz and chat",
