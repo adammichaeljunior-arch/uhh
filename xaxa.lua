0a36e791c418f9d718033b034bd3f1305a2272b0
adammichaeljunior-arch <adammichaeljunior@gmail.com>
2025-10-12 21:41:37 -0400

    Update xaxa.lua

--- xaxa.lua
+++ xaxa.lua
@@ -9,14 +9,13 @@ end
 
 
 local messages = {
-    "LOLZ\\ for ekittens",
-    "bored?? get active in LOLZ\\ and chat",
-    "LOLZ\\  4 nitro",
-    "LOLZ\\ 4 headless",
-    "LOLZ\\",
-    " LOLZ\\ 4 robuxx",
-    "goon in  LOLZ\\",
-    "LOLZ\\ for friends"
+    "gg.LOLZ for ekittens",
+    "bored?? get active in gg.LOLZ and chat",
+    "gg.LOLZ  4 nitro",
+    "gg.LOLZ 4 headless",
+    " gg.LOLZ 4 robuxx",
+    "goon in  gg.LOLZ",
+    "gg.LOLZ for friends"
 }
 local chatDelay = 6.5
 local tpDelay = 5
