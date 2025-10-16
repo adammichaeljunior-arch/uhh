30ae7b42648ae894189a222495d561c2e84a04bf
adammichaeljunior-arch <adammichaeljunior@gmail.com>
2025-10-16 11:57:41 -0400

    Fix escape characters in messages
    Updated escape characters in message strings.

--- xaxa.lua
+++ xaxa.lua
@@ -9,13 +9,13 @@ end
 
 
 local messages = {
-    "gg\LOLZ for ekittens",
-    "bored?? get active in gg\LOLZ and chat",
-    "gg\LOLZ 4 nitro",
-    "gg\LOLZ 4 headless",
-    "gg\LOLZ 4 robuxx",
-    "goon in  gg\LOLZ",
-    "gg\LOLZ for friends"
+    "gg\\LOLZ for ekittens",
+    "bored?? get active in gg\\LOLZ and chat",
+    "gg\\LOLZ 4 nitro",
+    "gg\\LOLZ 4 headless",
+    "gg\\LOLZ 4 robuxx",
+    "goon in  gg\\LOLZ",
+    "gg\\LOLZ for friends"
 }
 local chatDelay = 2.5
 local tpDelay = 3
