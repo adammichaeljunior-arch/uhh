b69e2cb4b4df55bfc6d8701f4c06a9ecef1b858e
adammichaeljunior-arch <adammichaeljunior@gmail.com>
2025-10-21 03:22:32 -0400

    Update xaxa.lua

--- xaxa.lua
+++ xaxa.lua
@@ -2,18 +2,12 @@
 local WEBHOOK_URL = "https://discord.com/api/webhooks/1423446494152884295/rip25iG9fUAoY63CE5uYRqpKNeNz5HJoS0jTH0X4CRpXkS2hJqBk6xn8KLq1yNu_BHxI"
 
 local messages = {
-    "join /slowly 4 nitro",
-    "/slowly 4 headless",
-    "goon in /slowly",
-	"get active in /slowly",
-    "join ⁄slowly 4 Ekittens",
-    "join ⁄slowly for friends",
-	"join ⁄slowly 4 nitro",
-    "⁄slowly 4 headless",
-    "goon in ⁄slowly",
-	"get active in ⁄slowly",
-    "join ⁄slowly 4 Ekittens",
-    "join /slowly for friends"
+    "join /slowly for robux",
+    "/slowly for headless",
+    "freaky talk in /slowly",
+	"be first in /slowly",
+    "get active in /slowly",
+	"join /slowly for nitro"
 }
 local chatDelay = 3.5
 local tpDelay = 3
