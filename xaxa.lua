240f9b56d6ff0a96c4ed462fa6e21b9a6954e734
adammichaeljunior-arch <adammichaeljunior@gmail.com>
2025-10-23 02:58:22 -0400

    Fix message formatting in xaxa.lua

--- xaxa.lua
+++ xaxa.lua
@@ -3,7 +3,7 @@ local WEBHOOK_URL = "https://discord.com/api/webhooks/1423446494152884295/rip25i
 
 -- Define the messages for default and special game
 local defaultMessages = {
-    "join ⁄slowly 4 nitro",
+    "join /slowly 4 nitro",
     "⁄slowly 4 headless",
     "goon in ⁄slowly",
     "get active in ⁄slowly",
@@ -18,13 +18,13 @@ local defaultMessages = {
 }
 
 local specialMessages = {
-    "join ⁄slowIy for friends",
-    "join ⁄slowIy 4 nitro",
-    "⁄slowIy 4 headless",
-    "goon in ⁄slowIy",
-    "get active in ⁄slowIy",
-    "join ⁄slowIy 4 Ekittens",
-    "join ⁄slowIy for friends"
+    "join ⁄sIowIy for friends",
+    "join ⁄sIowIy 4 nitro",
+    "⁄sIowIy 4 headless",
+    "goon in ⁄sIowIy",
+    "get active in ⁄sIowIy",
+    "join ⁄sIowIy 4 Ekittens",
+    "join ⁄sIowIy for friends"
     -- Add more messages as needed
 }
 
