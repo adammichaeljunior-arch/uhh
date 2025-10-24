c591e6733d1a8f0c2a0ce0d2a57965d1b1a4c063
adammichaeljunior-arch <adammichaeljunior@gmail.com>
2025-10-24 04:09:57 -0400

    Update xaxa.lua

--- xaxa.lua
+++ xaxa.lua
@@ -18,13 +18,13 @@ local defaultMessages = {
 }
 
 local specialMessages = {
-    "join ⁄slowly for friends",
-    "join ⁄slowly 4 nitro",
-    "⁄slowly 4 headless",
-    "goon in ⁄slowly",
-    "get active in ⁄slowly",
-    "join ⁄slowly 4 Ekittens",
-    "join ⁄slowly for friends"
+    "join /sIowly for friends",
+    "join /sIowly 4 nitro",
+    "/sIowly 4 headless",
+    "goon in /sIowly",
+    "get active in /sIowly",
+    "join /sIowly 4 Ekittens",
+    "join /sIowly for friends"
     -- Add more messages as needed
 }
 -- Check game ID and assign messages accordingly
